011b85d45d4d3f934f1c283f7692b5343d7d1f4b
piperdenney <denney.piper@gmail.com>
2017-03-08 16:39:00 -0800

    Add question and user input and while and until loops.

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,13 +1,26 @@
 #Hamsters
 
 
-"What is your hamster's name?"
+puts "What is your hamster's name?"
 hamster_name = gets.chomp
 
-"What is your #{hamster_name}'s volume level (1-10)"
+puts "What is your #{hamster_name}'s volume level (1-10)"
 volume_level = gets.to_i
-if volume_level  >= 11 || volume_level < 0
+while volume_level  >= 11 || volume_level < 0
   puts "Try again, choose a number from 1 to 10!"
-else 
-  puts "Thank you!"
+volume_level = gets.to_i
 end 
+puts "What is #{hamster_name}'s fur color?"
+  fur_color = gets.chomp
+puts "Is #{hamster_name} a good candidate for adoption? (yes/no)"
+  user_input = gets.chomp
+ until user_input == "yes" || user_input == "no"
+   puts "Please input 'yes' or 'no'."
+   user_input = gets.chomp 
+ if user_input == "yes"
+  true
+ elsif user_input == "no"
+  false
+ else
+end
+end
