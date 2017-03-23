c39614c497aaac3e5ca3055a8c1038a8ef4afbe6
piperdenney <denney.piper@gmail.com>
2017-03-22 21:34:09 -0700

    Add translate that translates human to dog.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -9,20 +9,35 @@ class Puppy
   def speak(number)
    number.times do 
     puts "Woof!"
-   end
   end
- 
- def roll_over
+end
+
+def roll_over
   p "*rolls over*"
- end
+end
 
 
- def dog_years(human_years)
+def dog_years(human_years)
   dog_age = human_years * 7
   p "In dog years the puppy is #{dog_age}"
- end
+end
+
+ #Method that translates a human word into dog language 
+ def translate(human_word)
+  #empty array for dog word
+  dog_speak = []
+  #for each letter in human word
+  human_word.chars.each do |letter|
+  #move to the next letter in alphabet and put into empty dog speak array
+  dog_speak << letter.next
 end 
- 
+#print array joined back to string
+p dog_speak.join
+end
+
+
+end 
+
 
 
 
@@ -35,3 +50,4 @@ patch.fetch("squirrel")
 patch.speak(5)
 patch.roll_over
 patch.dog_years(3)
+patch.translate("cat")
