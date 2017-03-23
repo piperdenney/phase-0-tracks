48c6271ef38c8003e01faf7615a317411270b325
piperdenney <denney.piper@gmail.com>
2017-03-23 14:25:23 -0700

    Add array with example genders and ethnicities. Add array that stores instances of santa. iterate through length of gender array and create new instance of santa for each with gender and ethnicity from arrays.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -16,9 +16,28 @@ class Santa
     p "That was an amazing #{cookie_type} cookie!"
   end 
 
-
+  def print_information
+    p "Gender: #{@gender} Ethnicity: #{@ethnicity}"
+  end
 end
 
-clause = Santa.new 
-clause.speak
-clause.eat_milk_and_cookies("chocolate chip")
+# clause = Santa.new 
+# clause.speak
+# clause.eat_milk_and_cookies("chocolate chip")
+
+#create empty array called santas
+santas = []
+#array of possible genders
+example_genders = ["transgender", "gender queer", "non binary", "female", "cisgender", "gender fluid"]
+#array of possible ethnicities
+example_ethnicities = ["black", "latinX", "N/A", "Japanese", "caucasian", "Scandinavian"]
+#For the length of how many genders there are do this with i
+example_genders.length.times do |i|
+  #Create a new instance of santa with example gender(i) and example ethnicity(i)
+  #and push it into santa array
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end 
+
+santas.each do |i|
+  i.print_information
+end
