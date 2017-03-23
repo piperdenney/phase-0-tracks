8071aecc0f74b7a021dd90720eb482fec10d7cda
piperdenney <denney.piper@gmail.com>
2017-03-22 21:23:03 -0700

    Add 3 methods for dog class. Test in driver code.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,33 @@ class Puppy
     toy
   end
 
-end
 
+  def speak(number)
+   number.times do 
+    puts "Woof!"
+   end
+  end
+ 
+ def roll_over
+  p "*rolls over*"
+ end
+
+
+ def dog_years(human_years)
+  dog_age = human_years * 7
+  p "In dog years the puppy is #{dog_age}"
+ end
+end 
+ 
+
+
+
+
+
+#Driver Code
+
+patch = Puppy.new 
+patch.fetch("squirrel")
+patch.speak(5)
+patch.roll_over
+patch.dog_years(3)
