c1ea6433ce8f7dc56eaa3e2869675fc8017d91c4
piperdenney <denney.piper@gmail.com>
2017-03-25 12:25:27 -0700

    Add 4 methods that match with rspec tests.

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,6 +1,6 @@
 class TodoList
 
-def initialize(list)
+def initialize(task_list)
   @task_list = task_list
 end
 
@@ -12,4 +12,13 @@ def add_item(new_task)
   @task_list << new_task
 end
 
+def delete_item(task_to_delete)
+  @task_list.delete(task_to_delete)
+end 
+
+def get_item(index)
+    @task_list[index]
 end
+
+end
+
